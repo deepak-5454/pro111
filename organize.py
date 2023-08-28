49da498aba440b150862a34f2f3a0dadf8b3cbfb
deepak-5454 <111001426+deepak-5454@users.noreply.github.com>
2023-08-28 20:15:24 +0530

    Add files via upload

---
+++ organize.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+
+from_dir="C:/Users/deepa/Downloads"
+to_dir="C:/Users/deepa/OneDrive/Desktop/downloadedimages"
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
